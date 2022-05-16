5be3795b5e261273a009c16795763fa1d248bbd0
giorgirogava <giorgirogava01ai@gmail.com>
2022-05-16 13:32:32 +0200

    - init

---
+++ Exam 1/Custom View/AvatarView.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+
+
+class AvatarView:UIView{
+    
+    
+    @IBOutlet weak var imageAvatar: UIImageView!
+    
+    
+    @IBAction func didTap(_ sender: Any) {
+        
+        
+    }
+    
+    
+    @IBOutlet var contentView: UIView!
+    
+//
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        setup()
+//    }
+//
+//
+//    required init?(coder: NSCoder) {
+//        super.init(coder: coder)
+//        setup()
+//    }
+//
+//
+//
+//    func setup() {
+//
+//        Bundle.main.loadNibNamed("AvatarView", owner: self)
+//        self.addSubview(contentView)
+//
+//
+////        contentView.translatesAutoresizingMaskIntoConstraints = true
+//        contentView.frame = self.frame
+//
+//
+//    }
+//
+    
+}
--- Exam 1/ViewController.swift
+++ Exam 1/ViewController.swift
@@ -1,19 +1,71 @@
-//
-//  ViewController.swift
-//  Exam 1
-//
-//  Created by Giorgi Rogava on 16.05.22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    var count:Int = 0
+    
+    @IBOutlet weak var imageAvatar: UIImageView!
+    
+    
+    @IBAction func didTap(_ sender: Any) {
+        
+        imageAvatar?.layer.cornerRadius = (imageAvatar?.frame.size.width ?? 0.0) / 2
+        imageAvatar?.clipsToBounds = true
+        imageAvatar?.layer.borderWidth = 2
+        imageAvatar?.layer.borderColor = UIColor.white.cgColor
+       
+        switch count {
+            
+        case 0:
+            imageAvatar.image = UIImage(named: "Skin")
+            count = count + 1
+            
+        case 1:
+            imageAvatar.image = UIImage(named: "Swift")
+            count = count + 1
+            
+        default:
+            imageAvatar.image = UIImage(named: "Endurance")
+            count = 0
+            
+        }
+        
+    }
+    
+    
+    @IBOutlet weak var imageAvatar2: UIImageView!
+    
 
-
+    @IBAction func didTap2(_ sender: Any) {
+        
+        imageAvatar2?.layer.cornerRadius = (imageAvatar2?.frame.size.width ?? 0.0) / 2
+        imageAvatar2?.clipsToBounds = true
+        imageAvatar2?.layer.borderWidth = 2
+        imageAvatar2?.layer.borderColor = UIColor.white.cgColor
+      
+        switch count {
+     
+        case 0:
+            imageAvatar2.image = UIImage(named: "skin2")
+            count = count + 1
+            
+        case 1:
+            imageAvatar2.image = UIImage(named: "skin3")
+            count = count + 1
+            
+        default:
+            imageAvatar2.image = UIImage(named: "skin1Wick")
+            count = 0
+            
+        }
+        
+    }
+    
 }
-
